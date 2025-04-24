3196133fa984c9997b51f088b803bcfb7a0c551f
Ulysse Lacour <contact@ulysselacour.com>
2025-04-24 08:28:34 +0200

    Initial boilerplate

---
+++ components/MyScene.client.vue
@@ -0,0 +1,82 @@
+<template>
+	<TresCanvas v-bind="gl" window-size>
+		<TresPerspectiveCamera :position="[0, 1.7, 7]" :look-at="[0, 0, 0]" />
+
+		<OrbitControls :enabled="config.orbitControlsEnabled" />
+
+		<SampleBox :position="[-2, 0, 0]" />
+
+		<Suspense>
+			<Suzanne :position="[2, 0, 0]" />
+		</Suspense>
+
+		<Suspense>
+			<StatsGl />
+		</Suspense>
+	</TresCanvas>
+</template>
+
+<script setup>
+import { OrbitControls, StatsGl } from '@tresjs/cientos'
+import { Pane } from 'tweakpane'
+
+//
+// Refs
+//
+const sampleStore = useSampleStore()
+
+const config = reactive({
+	orbitControlsEnabled: true,
+})
+
+const gl = reactive({
+	clearColor: '#1C1C1C',
+	powerPreference: 'high-performance',
+})
+
+//
+// Lifecycle
+//
+onMounted(async () => {
+	await nextTick()
+
+	// Do stuff on mount
+	createDebugPane()
+})
+
+//
+// Functions
+//
+function createDebugPane() {
+	const pane = new Pane()
+
+	pane.title = 'Configuration'
+
+	pane.addBinding(gl, 'clearColor', { label: 'Clear Color', colorMode: 'hex' })
+
+	pane.addBlade({
+		view: 'separator',
+	})
+
+	pane.addBinding(config, 'orbitControlsEnabled', {
+		label: 'Orbit Controls enabled',
+	})
+
+	pane.addBlade({
+		view: 'separator',
+	})
+
+	pane.addButton({ title: 'Increase counter (Pinia)' }).on('click', () => {
+		sampleStore.increment()
+	})
+
+	pane.addButton({ title: 'Decreate counter (Pinia)' }).on('click', () => {
+		sampleStore.decrement()
+	})
+
+	pane.addBinding(sampleStore, 'count', {
+		label: 'Pinia counter',
+		readonly: true,
+	})
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Nuxt 3 + TresJS starter",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Starter template for Nuxt 3 + TresJS created by Francesco Michelini",
+        },
+      ],
+    },
+  },
+
+  css: ["@/assets/css/index.scss"],
+
+  modules: [
+    "@nuxt/devtools",
+    "@tresjs/nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        prefetch: true,
+        download: true,
+        families: {
+          "Maven+Pro": [400, 500, 700],
+        },
+      },
+    ],
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ],
+  ],
+
+  imports: {
+    dirs: ["stores"],
+  },
+
+  tres: {
+    devtools: true,
+    glsl: true,
+  },
+});
